15189bd18a97110b1d8345d87a7cabb66c942017
Lalit Aditya <workharderlalit@gmail.com>
2025-09-04 00:42:45 +0530

    getting file ready for deployment

--- run.py
+++ run.py
@@ -4,3 +4,8 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # 8000 for local, Cloud Run sets PORT automatically
+    app.run(host="0.0.0.0", port=port)
